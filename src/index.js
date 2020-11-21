8f1cca7471012cdc9107a158a6b0ecb0ae17e0fc
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-11-21 13:24:07 +0400

    Add the faker library

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className='ui container comments'>
       <div className='comment'>
         <a href='/' className='avatar'>
-          <img src='' alt='avatar' />
+          <img src={faker.image.animals()} alt='avatar' />
         </a>
-        <div className="content">
-          <a href="/" className="author">
+        <div className='content'>
+          <a href='/' className='author'>
             Sam
           </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+          <div className='metadata'>
+            <span className='date'>Today at 6:00PM</span>
           </div>
-          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, commodi.</div>
+          <div className='text'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+            commodi.
+          </div>
         </div>
       </div>
     </div>
