6df9cc44f663b1fd96466c13430db42974421df3
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-11-21 13:03:39 +0400

    Add the scaffolding for the comment section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>
+  return (
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img src='' alt='avatar' />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, commodi.</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />,document.getElementById('root'));
